0489880fe25599dc004529cdd3850b3e8af220b6
Seggi SMS <seggimarugira@gmail.com>
2022-08-29 14:00:00 +0200

    add read data from database

--- php/src/objects/product.php
+++ php/src/objects/product.php
@@ -24,4 +24,44 @@ class Product
         $stmt->execute();
         return $stmt;
     }
+
+    function readOne()
+    {
+        $query = "SELECT
+        c.name as category_name, p.id, p.name, p.description, p.price, p.category_id, p.created
+        FROM
+        " . $this->table_name . " p
+        LEFT JOIN 
+        categories c ON p.category_id = c.id WHERE p.id = ? LIMIT 0,1";
+
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(1, $this->id);
+        $stmt->execute();
+
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        $this->name = $row['name'];
+        $this->price = $row['price'];
+        $this->description = $row['description'];
+        $this->category_id = $row['category_id'];
+        $this->category_name = $row['category_name'];
+    }
+
+    function create()
+    {
+        $query = "INSERT INTO " . $this->table_name . " SET name=:name, price=:price, description=:description, category_id=:category_id, created=:created";
+        $stmt = $this->conn->prepare($query);
+
+        $stmt->bindParam(":name", $this->name);
+        $stmt->bindParam(":price", $this->price);
+        $stmt->bindParam(":description", $this->description);
+        $stmt->bindParam(":category_id", $this->category_id);
+        $stmt->bindParam(":created", $this->created);
+
+        if ($stmt->execute()) {
+            return true;
+        }
+
+        return false;
+    }
 }
